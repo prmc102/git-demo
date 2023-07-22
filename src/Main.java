f3d7bec2aae2baecb768b2f2d46953ba9385d9c1
prmc102 <primecommunity102@gmail.com>
2023-07-22 12:44:08 +0100

    Feature commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Prime Testing");
+        System.out.println("Prime Testing2");
     }
 }
